a37c8dc24a1811fed7330b207c057986c9f7d4b9
Nolinnomen <syoon1101@gmail.com>
2025-07-31 17:44:48 +0900

    0731

--- Sample2/Assets/Scripts/Unity Attribute/UnityEventSample.cs
+++ Sample2/Assets/Scripts/Unity Attribute/UnityEventSample.cs
@@ -6,6 +6,7 @@ public class UnityEventSample : MonoBehaviour
     //Tooltip은 인스펙터에서 필드 값에 마우스를 올렸을때 설명을 보여주는 기능
     [Tooltip("이벤트 리스트를 추가하고, 실행할 기능을 가진 게임 오브젝트를 등록하세요")]
     public UnityEvent action;
+    
 
     private void Update()
     {
--- Sample2/Assets/Scripts/Unity Input/UnityIRandom.cs
+++ Sample2/Assets/Scripts/Unity Input/UnityIRandom.cs
@@ -11,6 +11,8 @@ public class UnityIRandom : MonoBehaviour
         //최소값 범위포함~ 최대값 포함X (정수일경우)
 
         //최소값 범위 포함~ 최대값 포함O (실수일경우)
-        rand = Random.Range(0, 11);//0~10
+        rand = Random.Range(0, 10);//0~9
+        // 0 1 2 3 4 5 6 7 8 9
+        // 이중에서 9보다 작은 값이 뽑힐 확률? 90%
     }
 }
